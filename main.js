a800a516fc1068749bcf7ad4c7337f7d7aeea24d
AdiChidambaram <72016974+AdiChidambaram@users.noreply.github.com>
2020-12-31 14:22:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var heads = [
+    "Chidambaram Murgapa",
+    "Indhu Chidambram",
+    "Aditya Chidambaram",
+   "Anvita Chidambaram" 
+];
+
+var ima = [
+    "Dad.jpg.jfif",
+    "Mom.jpg.jpg",
+    "Me.jpg.png",
+    "Sister.jpg.jpg"
+];
+i = 0;
+function ne(){
+
+document.getElementById("people").src = ima[i];
+document.getElementById("person").innerHTML = heads[i];
+i++;
+}
+
+
+
+
+
+    
+
+
+
+
